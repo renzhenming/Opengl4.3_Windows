75e54093696cbfee0e2f05e0925832e7b4a7198f
renzhenming <1565766177@qq.com>
2025-03-23 11:21:43 +0800

    绘制彩色立方体

--- 07.draw_cube_colorfull.cpp
+++ 07.draw_cube_colorfull.cpp
@@ -10,27 +10,27 @@
 #include "Utils.h"
 using namespace std;
 
-#define numVAOs6 1
-#define numVBOs6 2
+#define numVAOs7 1
+#define numVBOs7 2
 
 
-GLuint vao6[numVAOs6];
-GLuint vbo6[numVBOs6];
+GLuint vao7[numVAOs7];
+GLuint vbo7[numVBOs7];
 
-GLuint program;
-GLuint mvLoc;
-GLuint projLoc;
+GLuint program7;
+GLuint mvLoc7;
+GLuint projLoc7;
 
-float cameraX, cameraY, cameraZ;
-float cubeX, cubeY, cubeZ;
+float cameraX7, cameraY7, cameraZ7;
+float cubeX7, cubeY7, cubeZ7;
 
-int width;
-int height;
-float aspect;
+int width7;
+int height7;
+float aspect7;
 
-glm::mat4 pMat, vMat, mMat, mvMat;
+glm::mat4 pMat7, vMat7, mMat7, mvMat7;
 
-void setupVertices(void) {
+void setupVertices7(void) {
 	float vertexPositions[108] = {
 		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
 		1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
@@ -46,41 +46,41 @@ void setupVertices(void) {
 		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
 	};
 
-	glGenVertexArrays(numVAOs6, vao6);
-	glBindVertexArray(vao6[0]);
-	glGenBuffers(numVBOs6, vbo6);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo6[0]);
+	glGenVertexArrays(numVAOs7, vao7);
+	glBindVertexArray(vao7[0]);
+	glGenBuffers(numVBOs7, vbo7);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo7[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 }
-void init6(GLFWwindow* window) {
-	program = Utils::createShaderProgram("vertexShader.cube", "fragShader.cube");
-	cameraX = 0.0f;
-	cameraY = 0.0f;
-	cameraZ = 8.0f;
-	cubeX = 0.0f;
-	cubeY = -2.0f;
-	cubeZ = 0.0f;
-	setupVertices();
+void init7(GLFWwindow* window) {
+	program7 = Utils::createShaderProgram("vertexShader.cube_colorfull", "fragShader.cube_colorfull");
+	cameraX7 = 0.0f;
+	cameraY7 = 0.0f;
+	cameraZ7 = 8.0f;
+	cubeX7 = 0.0f;
+	cubeY7 = -2.0f;
+	cubeZ7 = 0.0f;
+	setupVertices7();
 }
 
 
-void display6(GLFWwindow* window,double currentTime) {
+void display7(GLFWwindow* window, double currentTime) {
 	glClear(GL_DEPTH_BUFFER_BIT);
-	glUseProgram(program);
-	mvLoc = glGetUniformLocation(program, "mv_matrix");
-	projLoc = glGetUniformLocation(program, "proj_matrix");
-	glfwGetFramebufferSize(window, &width, &height);
-	aspect = (float)width / (float)height;
-	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+	glUseProgram(program7);
+	mvLoc7 = glGetUniformLocation(program7, "mv_matrix");
+	projLoc7 = glGetUniformLocation(program7, "proj_matrix");
+	glfwGetFramebufferSize(window, &width7, &height7);
+	aspect7 = (float)width7 / (float)height7;
+	pMat7 = glm::perspective(1.0472f, aspect7, 0.1f, 1000.0f);
 
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeX, cubeY, cubeZ));
-	mvMat = vMat * mMat;
+	vMat7 = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX7, -cameraY7, -cameraZ7));
+	mMat7 = glm::translate(glm::mat4(1.0f), glm::vec3(cubeX7, cubeY7, cubeZ7));
+	mvMat7 = vMat7 * mMat7;
 
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+	glUniformMatrix4fv(mvLoc7, 1, GL_FALSE, glm::value_ptr(mvMat7));
+	glUniformMatrix4fv(projLoc7, 1, GL_FALSE, glm::value_ptr(pMat7));
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo6[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo7[0]);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 
@@ -89,21 +89,21 @@ void display6(GLFWwindow* window,double currentTime) {
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
-int main6(void) {
+int main(void) {
 	if (!glfwInit()) {
 		exit(EXIT_FAILURE);
 	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow *window = glfwCreateWindow(1000, 600, "rzm titie", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1000, 600, "rzm titie", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) {
 		exit(EXIT_FAILURE);
 	}
 	glfwSwapInterval(1);
-	init6(window);
+	init7(window);
 	while (!glfwWindowShouldClose(window)) {
-		display6(window, glfwGetTime());
+		display7(window, glfwGetTime());
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
